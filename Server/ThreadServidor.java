f682135342e86ede9a99d5742d10c6797a6bcbaf
MauAguero-12 <76414021+MauAguero-12@users.noreply.github.com>
2021-01-26 18:11:24 -0600

    Add files via upload

--- Server/ThreadServidor.java
+++ Server/ThreadServidor.java
@@ -16,17 +16,23 @@ public class ThreadServidor extends Thread{
     private boolean running = true;
     Servidor server;
     protected boolean listo = false;
+    public int dinero = 4000;
+    public int numJugador;
+    public int mundos = 1;
+    public int mercados = 1;
 
     public ThreadServidor(Socket socketRef, Servidor server) throws IOException {
         this.socketRef = socketRef;
         reader = new DataInputStream(socketRef.getInputStream());
         writer = new DataOutputStream(socketRef.getOutputStream());
         this.server = server;
+        numJugador = server.conexiones.size();
     }
     
     public void enviarTurnoInicial() throws IOException{
         writer.writeInt(1);
         writer.writeUTF(nombre);
+        writer.writeInt(dinero);
         writer.writeInt(server.conexiones.size() - 1);
         
         for(int i = 0; i < server.conexiones.size() - 1; i++){
@@ -63,6 +69,7 @@ public class ThreadServidor extends Thread{
                             current.writer.writeUTF(mensaje);
                         }
                     break;
+                    
                     case 3: // ataque
                         int x = reader.readInt();
                         int y = reader.readInt();
@@ -79,12 +86,72 @@ public class ThreadServidor extends Thread{
                             current.writer.writeUTF(arma);
                         }
                     break;
+                    
                     case 4: // iniciar partida
                         listo = !listo;
                         server.iniciarPartida();
-                        
-                        
                     break;
+                    
+                    case 7: // construir
+                        x = reader.readInt();
+                        y = reader.readInt();
+                        String comp = reader.readUTF();
+                        boolean construccion  = false;
+                        
+                        if (comp.equals("Mundo")){
+                            if (mundos != 0){
+                                mundos--;
+                                construccion = true;
+                            } else if (dinero >= 120000){
+                                dinero -= 120000;
+                                construccion = true;
+                            }
+                        }
+                        
+                        if (comp.equals("Conector")){
+                            if (dinero >= 100){
+                                dinero -= 100;
+                                construccion = true;
+                            }
+                        }
+                        
+                        if (comp.equals("Mercado")){
+                            if (mercados != 0){
+                                mercados--;
+                                construccion = true;
+                            } else if (dinero >= 2000){
+                                dinero -= 2000;
+                                construccion = true;
+                            }
+                        }
+                        
+                        if (comp.equals("Mina")){
+                            if (dinero >= 1000){
+                                dinero -= 1000;
+                                construccion = true;
+                            }
+                        }
+                        
+                        if (comp.equals("Armería")){
+                            if (dinero >= 1500){
+                                dinero -= 1500;
+                                construccion = true;
+                            }
+                        }
+                        
+                        if (comp.equals("Templo")){
+                            if (dinero >= 2500){
+                                dinero -= 2500;
+                                construccion = true;
+                            }
+                        }
+                        
+                        writer.writeInt(7);
+                        writer.writeInt(x);
+                        writer.writeInt(y);
+                        writer.writeUTF(comp);
+                        writer.writeInt(dinero);
+                        writer.writeBoolean(construccion);
                 }
             } catch (IOException ex) {
                 
